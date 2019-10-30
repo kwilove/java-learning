33ed8f9c9555c8adcb39a4418cbfe49dc2ff674c
zjhuang <zjhuang@sheendu.com>
2019-10-30 18:18:14 +0800

    feat：新增分布式模块

---
+++ dist-tech/src/main/java/com/zjhuang/disttech/DistTechApplication.java
@@ -0,0 +1,13 @@
+package com.zjhuang.disttech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistTechApplication.class, args);
+    }
+
+}
